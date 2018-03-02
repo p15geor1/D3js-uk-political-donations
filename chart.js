c12b231d09c523c70c55dc9f01f28edd57ba71ec
p15geor1 <p15geor1@ionio.gr>
2018-03-02 15:03:27 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -285,18 +285,22 @@ function moveToFunds(alpha) {
 
 function moveToLetters(alpha){
 	return function(d) {
-		
-			if (d.value <= 100001) {
-				centreY = svgCentre.y
-				centreX = svgCentre.x + 75; 
+		var centreX = svgCentre.x + 75;
+			if (d.value <= 25001) {
+				centreY = svgCentre.y + 75;
+			} else if (d.value <= 50001) {
+				centreY = svgCentre.y + 55;
+			} else if (d.value <= 100001) {
+				centreY = svgCentre.y + 35;
 			} else  if (d.value <= 500001) {
-				centreY = svgCentre.y 
-				centreX = svgCentre.x + 150; 
-			} else  (d.value <= maxVal) {
-				centreY = svgCentre.y 
-				centreX = svgCentre.x + 400; 
+				centreY = svgCentre.y + 15;
+			} else  if (d.value <= 1000001) {
+				centreY = svgCentre.y - 5;
+			} else  if (d.value <= maxVal) {
+				centreY = svgCentre.y - 25;
+			} else {
+				centreY = svgCentre.y;
 			}
-		
 		d.x += (centreX - d.x) * (brake + 0.06) * alpha * 1.2;
 		d.y += (centreY - 100 - d.y) * (brake + 0.06) * alpha * 1.2;
 	};
