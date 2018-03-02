766fb066b67f771352676919ebf446ff0d0b97db
p15geor1 <p15geor1@ionio.gr>
2018-03-02 14:05:46 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -287,17 +287,17 @@ function moveToLetters(alpha){
 	return function(d) {
 		var centreX = svgCentre.x + 75;
 			if (d.value <= 25001) {
-				centreY = svgCentre.y + 75;
+				centreY = svgCentre.y 
 			} else if (d.value <= 50001) {
-				centreY = svgCentre.y + 55;
+				centreY = svgCentre.y 
 			} else if (d.value <= 100001) {
-				centreY = svgCentre.y + 35;
+				centreY = svgCentre.y 
 			} else  if (d.value <= 500001) {
-				centreY = svgCentre.y + 15;
+				centreY = svgCentre.y 
 			} else  if (d.value <= 1000001) {
-				centreY = svgCentre.y - 5;
+				centreY = svgCentre.y 
 			} else  if (d.value <= maxVal) {
-				centreY = svgCentre.y - 25;
+				centreY = svgCentre.y 
 			} else {
 				centreY = svgCentre.y;
 			}
